71ec84dc47da7b354a3d2a37a00f3417fe71a7ff
Xamul <luigi.zanderighi@gmail.com>
2026-02-25 10:33:43 +0100

    fix post refactor

--- scripts/replay-proxy.sh
+++ scripts/replay-proxy.sh
@@ -6,8 +6,8 @@ BACK_DIR=/home/htpc/backtrader
 DATE=${1:?"Uso: $0 YYYY-MM-DD [--speed N] [--log-dir DIR]"}
 shift  # Rimuovi la data, il resto va al proxy
 
-source $BACK_DIR/backtrader/backtrader/bin/activate
-cd $BACK_DIR/backtrader
+source $BACK_DIR/bt-gateway/.venv/bin/activate
+cd $BACK_DIR/bt-gateway/bt-alpaca-zmq
 
 echo "[replay-proxy] Data: $DATE, args: $@"
 exec python replay_zmq_proxy.py \
