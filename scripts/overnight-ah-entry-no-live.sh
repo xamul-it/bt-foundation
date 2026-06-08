2c76d56ad2b68ed3a77bded2456e680e98b14ce5
Xamul <luigi.zanderighi@gmail.com>
2026-06-08 21:20:15 +0200

    Set live OvernightAH exposure to 1.5

--- scripts/overnight-ah-entry-no-live.sh
+++ scripts/overnight-ah-entry-no-live.sh
@@ -6,5 +6,7 @@ set -euo pipefail
 export PSIM_ENV=live.key
 export AUCTION=False
 export TRADING_MODE=live
+export MAX_EXPOSURE=1.5
+export MARGIN_LEVERAGE=1.5
 
 exec /home/htpc/backtrader/scripts/overnight-ah-entry.sh "$@"
